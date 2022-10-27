1dd3f785bbcf4d7ea6ec3d7ddb59b3165b1a0737
MatejGalic <matey.galic99@gmail.com>
2022-10-27 21:23:31 +0200

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  auth0_client_domain: 'dev-0o15xqyhb5vt8cs0.us.auth0.com', //TODO: stavit ovo negdje drugdje
+  auth0_client_id: 'SilV3mXGYZOCW2bqBtm5sVgLjK2vFL1A',
 };
