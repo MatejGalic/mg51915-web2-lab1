a514d727dbc00c92d2887385198de4b2c451f7a0
MatejGalic <matey.galic99@gmail.com>
2022-10-29 13:34:17 +0200

    Revert fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  auth0_client_domain: process.env.CLIENT_DOMAIN,
-  auth0_client_id: process.env.CLIENT_ID,
+  auth0_client_domain: 'dev-0o15xqyhb5vt8cs0.us.auth0.com',
+  auth0_client_id: 'SilV3mXGYZOCW2bqBtm5sVgLjK2vFL1A',
 };
